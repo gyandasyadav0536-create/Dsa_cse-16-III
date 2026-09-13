10aa14d45d451f00b10bba15a21f56ec22527f8b
Dharamdas yadav <gyandasyadav0536@gmail.com>
2026-09-13 23:58:26 +0530

    first

--- dsaclass.cpp/next_greater_element.cpp
+++ dsaclass.cpp/next_greater_element.cpp
@@ -1,25 +1,39 @@
-#include<bits/stdc++.h>
-int main(){
-int arr={3,1,2,7,4,6,2,3};
-int n=sizeof(arr)/sizeof(arr[0]);
-for(int i=0;i<n;i++){
-cout<<arr[i];
-}
-cout<<endl;
-int nge[n];
-stack<int>st;
-nge[n-1]=-1;
-st.push(arr[n-1]);
-for(int i=n-2;i>=0;i--){
-while(st.size()>0 &&st.top()<=arr[i]);
-st.pop();
-if(st.size()== nge[i]=-1);
-else
-nge[i]=st.top();
-st.push(arr[i]);
-}
-for(int i=0;i<n;i++){
-cout<<nge[i]<<"";
-}
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int arr[] = {3,1,2,7,4,6,2,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    // Print original array
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int nge[n]; // Next Greater Element array
+    stack<int> st;
+
+    nge[n-1] = -1; // Last element has no NGE
+    st.push(arr[n-1]);
+
+    for(int i=n-2; i>=0; i--) {
+        while(!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+        if(st.empty()) {
+            nge[i] = -1;
+        } else {
+            nge[i] = st.top();
+        }
+        st.push(arr[i]);
+    }
+
+    // Print NGE array
+    for(int i=0; i<n; i++) {
+        cout << nge[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
